4571ad37f79288bbfb1a796d784b9bc13bce213b
Kevser <kevser.src@outlook.com>
2017-12-12 19:13:33 +0300

    cleanurl edit

--- scan.go
+++ scan.go
@@ -62,8 +62,11 @@ var done = xml.Unmarshal(usomUrlList, &t)
 clean up the url distorting characters
 */
 func Cleanurl(url string) string {
+	if strings.Contains(url, "https://") {
+		return Cleanurl(strings.Split(url, "https://")[1])
+	}
 	if strings.Contains(url, "http://") {
-		return strings.Split(url, "http://")[1]
+		return Cleanurl(strings.Split(url, "http://")[1])
 	}
 	if strings.ContainsAny(url, "/") {
 		return strings.Split(url, "/")[0]
